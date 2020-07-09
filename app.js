d978e48599c30c0b6cd3f436ab9b50291ed9e66e
Reina <r.clarkemitchell@gmail.com>
2020-07-08 23:03:12 -0400

    added my follow along

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const books = document.querySelectorAll('#book-list li .name');
 
-Array.from(books).forEach(function(book){
-  book.textContent += ' (Book title)';
-});
+console.log(books)
 
-const bookList = document.querySelector('#book-list');
-bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML content</p>';
+books.forEach((book) => book.textContent = "Test");
+
+
+let bookList = document.querySelector('#book-list')
+// bookList.innerHTML = '<h2>Books and more books</h2>'
+bookList.innerHTML += '<p>Adding HTML</p>'
