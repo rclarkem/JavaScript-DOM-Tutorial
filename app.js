bae16ec929d09c818dd1517db7d4bd6f10c5143f
Reina <r.clarkemitchell@gmail.com>
2020-07-09 00:57:01 -0400

    added my follow along code of parent and child nodes.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const bookList = document.querySelector('#book-list');
+const bookList = document.querySelector('#book-list')
 
-console.log('book list parent element:', bookList.parentElement);
-console.log('book list parent node:', bookList.parentNode);
+console.log(bookList.parentNode)
+console.log(bookList.parentElement.parentElement)
+
+
+console.log(bookList.childNodes)
+console.log(bookList.children)
 
 console.log('all node children:');
 Array.from(bookList.childNodes).forEach(function(node){
@@ -11,11 +15,4 @@ Array.from(bookList.childNodes).forEach(function(node){
 console.log('all element children:');
 Array.from(bookList.children).forEach(function(node){
   console.log(node);
-});
-
-const titles = bookList.querySelectorAll('.name');
-
-console.log('Book titles:');
-Array.from(titles).forEach(function(title){
-  console.log(title.textContent);
 });
