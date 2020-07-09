1bab30fce1824e6941e1dcc2dfb36f93041ebcf2
Reina <r.clarkemitchell@gmail.com>
2020-07-09 01:03:40 -0400

    added my follow along code of siblings.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const bookList = document.querySelector('#book-list');
+const bookList = document.querySelector('#book-list')
 
-console.log('#book-list next sibling:', bookList.nextSibling);
-console.log('#book-list next element sibling:', bookList.nextElementSibling);
-console.log('#book-list previous sibling:', bookList.previousSibling);
-console.log('#book-list previous element sibling:', bookList.previousElementSibling);
+console.log('next sibling',bookList.nextSibling)
+console.log('next element sibling',bookList.nextElementSibling)
+console.log('previous sibling',bookList.previousSibling)
+console.log('previous element sibling',bookList.previousElementSibling)
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too Cool for everyone</br>'
