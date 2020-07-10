cf36018e13a9b7a75a465b5f6d9d7674034869ec
Reina <r.clarkemitchell@gmail.com>
2020-07-09 23:40:54 -0400

    added my follow along to bubbling events and added my own function for adding new events to test it.

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-
-let bookList = document.querySelector('#book-list ul')
-
-// console.log(bookList)
-
-bookList.addEventListener('click', (e) => {
-  // console.log(e.target.parentElement)
-  if (e.target.className === 'delete') {
+const books = document.querySelector('#book-list ul')
+// console.log(books)
+books.addEventListener('click', (e) => {
+  // I used the .matches method instead of className in the chance that I want something else.
+  const deletebtn = e.target.matches('.delete')
+  if (deletebtn) {
     const li = e.target.parentElement
-    li.parentNode.removeChild(li)
+    books.removeChild(li)
   }
 })
+
+let newLi = createAndAppend('li', books, null,null,undefined)
+let text = createAndAppend('span', newLi, 'name', null, (e) => e.innerText = 'The Lord of The Rings')
+let deletebtn = createAndAppend('span', newLi, 'delete',null,(e) => e.textContent = 'delete')
+// const newLi = document.createElement('li')
+// const newSpanOne = document.createElement('span')
+// const newSpanTwo = document.createElement('span')
+// newSpanOne.textContent = 'The Lord of the Rings'
+// newSpanOne.classList.add('name')
+// newSpanTwo.textContent = 'delete'
+// newSpanTwo.classList.add('delete')
+// newLi.append(newSpanOne)
+// newLi.append(newSpanTwo)
+// books.append(newLi)
+
+function createAndAppend(tagElem, parent, className, id, cb) {
+  let element = document.createElement(tagElem)
+  parent.append(element)
+  if (className !== null) {
+    element.className = className
+  }
+  if (id !== null) {
+    element.id = id
+  }
+  if (cb !== undefined) {
+    cb(element)
+  }
+  return element
+}
