eb1b59c4eaa42e6a51b84d9891ffbd359b59f513
Reina <r.clarkemitchell@gmail.com>
2020-07-26 18:43:40 -0400

    my follow along to checkboxes in addition to adding a completed strikethrough feature.

--- app.js
+++ app.js
@@ -1,45 +1,77 @@
-const list = document.querySelector('#book-list ul');
-const forms = document.forms;
+const bookList = document.querySelector('#book-list ul');
+console.log(bookList);
+const bookForm = document.forms['add-book'];
+const hideBooks = document.querySelector('#hide');
+const bookRead = document.querySelectorAll('.book-complete');
 
-// delete books
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
-  }
+//delete Function
+bookList.addEventListener('click', function (e) {
+	if (e.target.matches('.delete')) {
+		deleteBooks(e.target);
+	} else if (e.target.matches('.book-complete')) {
+		completeRead(e.target);
+	}
 });
 
-// add books
-const addForm = forms['add-book'];
-addForm.addEventListener('submit', function(e){
-  e.preventDefault();
-
-  // create elements
-  const value = addForm.querySelector('input[type="text"]').value;
-  const li = document.createElement('li');
-  const bookName = document.createElement('span');
-  const deleteBtn = document.createElement('span');
-
-  // add text content
-  bookName.textContent = value;
-  deleteBtn.textContent = 'delete';
-
-  // add classes
-  bookName.classList.add('name');
-  deleteBtn.classList.add('delete');
-
-  // append to DOM
-  li.appendChild(bookName);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
+//hide All books
+hideBooks.addEventListener('change', (ev) => {
+	if (hideBooks.checked) {
+		bookList.style.display = 'none';
+	} else {
+		bookList.style.display = 'initial';
+	}
 });
 
-// hide books
-const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', function(e){
-  if(hideBox.checked){
-    list.style.display = "none";
-  } else {
-    list.style.display = "initial";
-  }
+//delete books
+function deleteBooks(ev) {
+	const li = ev.parentElement;
+	bookList.removeChild(li);
+}
+
+//complete read books
+function completeRead(ev) {
+	ev.addEventListener('change', () => {
+		let title = ev.parentElement.querySelector('.name');
+		if (ev.checked) {
+			title.style.textDecoration = 'line-through';
+		} else {
+			title.style.textDecoration = 'initial';
+		}
+	});
+}
+
+// add Book Function
+bookForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let text = e.target.title.value;
+	let newLI = createAndAppendElement('li', bookList, null, null, undefined);
+	createAndAppendElement('input', newLI, null, 'book-complete', (e) => {
+		e.setAttribute('type', 'checkbox');
+	});
+	createAndAppendElement('label', newLI, null, null, (e) => {
+		e.htmlFor = checkBox.className;
+	});
+	createAndAppendElement('span', newLI, null, 'name', (e) => {
+		e.textContent = text;
+	});
+	createAndAppendElement('span', newLI, null, 'delete', (e) => {
+		e.textContent = 'delete';
+	});
+	e.target.title.value = '';
 });
+
+//helper append function
+function createAndAppendElement(tag, parent, id, className, callback) {
+	const element = document.createElement(tag);
+	parent.append(element);
+	if (id !== null) {
+		element.id = id;
+	}
+	if (className !== null) {
+		element.className = className;
+	}
+	if (callback !== undefined) {
+		callback(element);
+	}
+	return element;
+}
